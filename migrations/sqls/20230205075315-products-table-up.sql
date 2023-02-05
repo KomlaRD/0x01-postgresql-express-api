6229df45e708da664928047d7ec32e56d47d58ba
komlaRD <ankueric1@gmail.com>
2023-02-05 17:25:46 +0000

    create products table

--- migrations/sqls/20230205075315-products-table-up.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE products (product_id SERIAL PRIMARY KEY NOT NULL, 
-  name VARCHAR(100),
-  price DECIMAL,
-  category VARCHAR(100));
---
+++ migrations/sqls/20230205171709-products-table-up.sql
@@ -0,0 +1,2 @@
+-- Create products table
+CREATE TABLE products (products_id SERIAL PRIMARY KEY, name VARCHAR(100), price DECIMAL, category VARCHAR(150));
