546f0b6c52fdf259d0d336ffeadd3f30a396918f
komlaRD <ankueric1@gmail.com>
2023-02-08 19:23:12 +0000

    create routes for users endpoint

---
+++ src/handlers/users.ts
@@ -0,0 +1,42 @@
+import express, {Application, Request, Response} from "express";
+import { User, UserStore } from "../models/user";
+
+const store = new UserStore();
+
+// Index route
+const index = async (req: express.Request, res: express.Response) => {
+    const users = await store.index();
+    res.json(users);
+};
+
+// Show route
+const show = async (req: express.Request, res: express.Response) => {
+    const user = await store.show(req.params.id);
+    res.json(user);
+};
+
+// Create route
+const create = async (req: express.Request, res: express.Response) => {
+    try {
+        const user: User = {
+            user_id: parseInt(req.params.id as string),
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: req.body.password
+        }
+        const newUser = await store.create(user);
+        res.json(newUser);
+    } catch (err) {
+        res.json(400);
+        throw new Error (`The user could not be created: ${err}`);
+    }
+}
+
+// Create user route function
+const user_routes = (app: Application) => {
+    app.get('/users', index);
+    app.get('/users/:id', show);
+    app.post('/users', create);
+}
+
+export default user_routes;
