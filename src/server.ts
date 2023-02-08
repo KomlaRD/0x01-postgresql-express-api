07dba18c5b6e5d28cbdab9d16b05c82b66829e95
komlaRD <ankueric1@gmail.com>
2023-02-08 19:24:14 +0000

    add users route to application

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import products_route from './handlers/products'
+import user_routes from './handlers/users'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
@@ -11,7 +12,11 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Welcome to my store backend')
 })
 
+// Product routes
 products_route(app);
+
+// User routes
+user_routes(app);
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
