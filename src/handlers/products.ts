19cec75d9b8032ffaf3fcc46cbb04b879715b343
komlaRD <ankueric1@gmail.com>
2023-02-08 06:22:26 +0000

    add handler methods for product model

---
+++ src/handlers/products.ts
@@ -0,0 +1,42 @@
+import express, {Request, Response} from "express";
+import { Product, ProductStore } from "../models/product";
+
+const store = new ProductStore();
+
+// Index route
+const index = async (req: express.Request, res: express.Response) => {
+    const products = await store.index();
+    res.json(products);
+};
+
+// Show route
+const show = async (req: express.Request, res: express.Response) => {
+    const products = await store.show(req.params.id);
+    res.json(products);
+};
+
+// Create route
+const create = async (req: express.Request, res: express.Response) => {
+    try {
+        const products: Product = {
+            product_id: parseInt(req.params.id as string),
+            name: req.body.name,
+            price: req.body.price,
+            category: req.body.category
+        }
+        const newProduct = await store.create(products);
+        res.json(newProduct);
+    } catch (err) {
+        res.status(400);
+        res.json(err);
+};
+}
+
+// Create product route function
+const products_route = (app: express.Application) => {
+    app.get('/products', index);
+    app.get('/products/:id', show);
+    app.post('/products', create);
+};
+
+export default products_route;
