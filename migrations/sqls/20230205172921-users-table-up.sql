b1bc590bc70f213f82bb73b581bf77d2058dad92
komlaRD <ankueric1@gmail.com>
2023-02-09 06:19:17 +0000

    add new column to users table

--- migrations/sqls/20230205172921-users-table-up.sql
+++ migrations/sqls/20230205172921-users-table-up.sql
@@ -1,2 +1,5 @@
 -- Create users table
 CREATE TABLE users (user_id SERIAL PRIMARY KEY, first_name VARCHAR(150), last_name VARCHAR(150), password VARCHAR);
+
+-- Add a new column called username
+ALTER TABLE users ADD COLUMN (username); 
