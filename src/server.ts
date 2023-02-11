dcd0f462315a2e1538e529168665974c10f15285
komlaRD <ankueric1@gmail.com>
2023-02-11 00:21:45 +0000

    add openapi documentation

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,19 @@ import bodyParser from 'body-parser'
 import products_route from './handlers/products'
 import user_routes from './handlers/users'
 import order_routes from './handlers/orders'
+import swagger_ui from "swagger-ui-express";
+import YAML from 'yamljs'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
 
+// Declare api document variable
+const doc = YAML.load('src/api_docs.yml');
+
 app.use(bodyParser.json())
+
+// Use the api document 
+app.use("/api-docs", swagger_ui.serve, swagger_ui.setup(doc));
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Welcome to my store backend')
