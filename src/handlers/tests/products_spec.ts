8e84ef2d044417aed20e0a0617c19c96ff776e74
komlaRD <ankueric1@gmail.com>
2023-02-11 13:47:54 +0000

    test endpoint of product routes

---
+++ src/handlers/tests/products_spec.ts
@@ -0,0 +1,24 @@
+// Import modules
+import app from "../../server";
+import supertest from "supertest";
+import products_route from "../products";
+
+const request = supertest(app.use(products_route));;
+
+// Test enpoint for products routes
+describe("Test endpoint responses", () => {
+  it("gets all products", async () => {
+    const response = await request.get("/products");
+    expect(response.status).toBe(200);
+  });
+
+  it("gets a particular product", async() => {
+    const response = await request.get("/products/:id");
+    expect(response.status).toBe(200);
+  })
+
+  it("posts a new product", async () => {
+    const response = await request.post("/products")
+    expect(response.status).toBe(401);
+  });
+});
