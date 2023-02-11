bae0bd490a942a505ec8624bcbb83321bfcd9119
komlaRD <ankueric1@gmail.com>
2023-02-11 13:47:17 +0000

    test endpoint of order route

---
+++ src/handlers/tests/orders_spec.ts
@@ -0,0 +1,16 @@
+// Import modules
+import app from "../../server";
+import supertest from "supertest";
+import order_routes from "../orders";
+
+
+// App routes
+const request = supertest(app.use(order_routes));
+
+// Test server
+describe("Test endpoint responses", () => {
+  it("shows the order for a particular user", async () => {
+    const response = await request.get("/orders/:id");
+    expect(response.status).toBe(200);
+  });
+});
