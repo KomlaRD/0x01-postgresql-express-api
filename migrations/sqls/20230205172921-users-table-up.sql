5a1c65b03c1eb35747b3b5f821900f70c046a474
komlaRD <ankueric1@gmail.com>
2023-02-12 00:58:45 +0000

    typo: correct query for adding new column

--- migrations/sqls/20230205172921-users-table-up.sql
+++ migrations/sqls/20230205172921-users-table-up.sql
@@ -2,4 +2,4 @@
 CREATE TABLE users (user_id SERIAL PRIMARY KEY, first_name VARCHAR(150), last_name VARCHAR(150), password VARCHAR);
 
 -- Add a new column called username
-ALTER TABLE users ADD COLUMN (username); 
+ALTER TABLE users ADD COLUMN username VARCHAR(150); 
