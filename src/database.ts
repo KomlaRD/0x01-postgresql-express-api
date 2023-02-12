05efff3287e024d4f222d321c2f457dedbb643cf
komlaRD <ankueric1@gmail.com>
2023-02-12 00:10:56 +0000

    add test database

--- src/database.ts
+++ src/database.ts
@@ -9,16 +9,35 @@ const {
     POSTGRES_HOST,
     POSTGRES_DB,
     POSTGRES_USER,
-    POSTGRES_PASSWORD
+    POSTGRES_PASSWORD,
+    POSTGRES_TEST_DB,
+    ENV
 } = process.env
 
-// Initialise a database object called client
-const client = new Pool({
+let client;
+
+console.log(ENV);
+
+// Test database
+if (ENV === 'test') {
+    client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+
+    })
+};
+
+// Development database
+if (ENV === 'dev') {
+    client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
 })
+};
 
 // Export database object
 export default client
