70c4b09990678e3cf9beb32af3333d080fbf2e87
komlaRD <ankueric1@gmail.com>
2023-02-18 22:18:00 +0000

    add insert sql query

--- src/models/order.ts
+++ src/models/order.ts
@@ -16,7 +16,7 @@ export class OrderStore {
       // Connect to database
       const conn = await client.connect();
       // SQL query to show an order
-      const sql = 'SELECT * FROM orders WHERE user_id = user_id';
+      const sql = "SELECT * FROM orders WHERE user_id = user_id";
       // Run query on the database
       const result = await conn.query(sql, []);
       const order = result.rows[0];
@@ -26,6 +26,25 @@ export class OrderStore {
       return order;
     } catch (error) {
       throw new Error(`Order was not found: ${error}`);
+    }
+  }
+  
+  // Create order for testing purposes
+  async create(): Promise<Order> {
+    try {
+      // Connect to database
+      const conn = await client.connect();
+      // SQL query to create a new product
+      const sql = `INSERT INTO orders (status, product_id, user_id) VALUES ('complete', 1, 1) RETURNING *`;
+      // Run query on the database
+      const result = await conn.query(sql, []);
+      const order = result.rows[0];
+      // Close the connection
+      conn.release();
+      // Return the order created
+      return order;
+    } catch (error) {
+      throw new Error(`Order was not created: ${error}`);
     }
   }
 }
