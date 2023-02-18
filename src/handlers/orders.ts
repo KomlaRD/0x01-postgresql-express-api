39075a3efa244b49cfddcf79c0ad4b4dd5b2e4b3
komlaRD <ankueric1@gmail.com>
2023-02-18 15:46:24 +0000

    add try/catch block and authentication verification

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -1,16 +1,21 @@
 import express from "express";
 import { OrderStore } from "../models/order";
+import verifyAuthToken  from "../middleware/authenticate";
 
 const store = new OrderStore();
 
 // Show route
 const show = async (req: express.Request, res: express.Response) => {
-    const orders = await store.show(req.params.user_id);
-    res.json(orders);
-};
+    try {
+        const orders = await store.show(req.params.user_id);
+        res.json(orders);
+} catch (err) {
+    throw new Error(`Unable to show order: ${err}`);} 
+    }
+    
 
 const order_routes = (app: express.Application) => {
-    app.get('/orders/:id', show);
+    app.get('/orders/:id', verifyAuthToken, show);
 }
 
 export default order_routes;
