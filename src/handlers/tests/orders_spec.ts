0dfca1ffe800c7556ac981561c8bb26d560b38e9
komlaRD <ankueric1@gmail.com>
2023-02-18 16:22:47 +0000

    change response status code to 401

--- src/handlers/tests/orders_spec.ts
+++ src/handlers/tests/orders_spec.ts
@@ -11,6 +11,6 @@ const request = supertest(app.use(order_routes));
 describe("Test endpoint responses", () => {
   it("shows the order for a particular user", async () => {
     const response = await request.get("/orders/:id");
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(401); // response is 200 when authentication is successful 
   });
 });
