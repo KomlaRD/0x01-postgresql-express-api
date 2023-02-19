767306deca890e5fa1ae25829c8f831dc0eac9b6
komlaRD <ankueric1@gmail.com>
2023-02-19 08:00:24 +0000

    add creating product and user before order is created

--- src/models/tests/order_spec.ts
+++ src/models/tests/order_spec.ts
@@ -1,22 +1,34 @@
 import { OrderStore } from "../order";
+import { ProductStore } from "../product";
+import { UserStore } from "../user";
 
-// Instantiate a new user store
-const store = new OrderStore();
+// Instantiate new stores
+const store_order = new OrderStore();
+const store_product = new ProductStore();
+const store_user = new UserStore();
 
 // Suite test
 describe("OrderStore", () => {
   beforeAll(async () => {
-    await store.create();
+    await store_product.create();
+    await store_user.create({
+      user_id: 1,
+      password: "silenthour",
+      first_name: "Eric",
+      last_name: "Anku",
+      username: "erico",
+    });
+    await store_order.create();
   });
 
   // Check for the existence of the show method
   it("should have a show method", () => {
-    expect(store.show).toBeDefined();
+    expect(store_order.show).toBeDefined();
   });
 
   // Test the return value of the show method
   it("show method should return an order for particular user", async () => {
-    const result = await store.show("1");
+    const result = await store_order.show("1");
     expect(result).not.toEqual({
       order_id: 1,
       status: "complete",
